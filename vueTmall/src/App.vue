121394f4de65e77d610e0bd83259c7b2fc530664
Jiahao LUO <2679229323@qq.com>
2018-03-25 11:37:46 +0100

    init tmall vue project

---
+++ vueTmall/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
